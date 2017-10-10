8b0eeec118fd8251b21108f3bc1bc6848235e4d4
yangji9181 <yangji9181@163.com>
2017-10-10 13:18:47 -0500

    rm .pyc

---
+++ batch.py
@@ -0,0 +1,12 @@
+import argparse
+import getpass
+import sys
+from predictor import Predictor
+
+
+if __name__ == '__main__':
+	predictor = Predictor(args)
+	predictor.train()
+	predictor.plot()
+	predictor.evaluate()
+	predictor.dump()
