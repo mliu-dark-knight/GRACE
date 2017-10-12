8161c5ccd84c2e7ff57599133e527cd56b048e0c
Mengxiong <mliu60@illinois.edu>
2017-10-12 01:29:45 -0500

    rm tensorboard file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import subprocess
 from config import args
 from predictor import Predictor
 
 
 if __name__ == '__main__':
+	subprocess.call('rm ' + args.model_dir + '*', shell=True)
 	predictor = Predictor(args)
 	predictor.train()
 	# predictor.plot()
