e5033988f6d7ad32b328be38e1a5435dfcfa3471
Mengxiong <mliu60@illinois.edu>
2017-10-14 16:11:09 -0500

    gpu allocation

--- experiment.py
+++ experiment.py
@@ -1,9 +1,6 @@
 import argparse
 import subprocess
-from multiprocessing import *
-
 import numpy as np
-
 from config import args
 from predictor import Predictor
 
@@ -11,41 +8,17 @@ from predictor import Predictor
 def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--num_exp', type=int, default=10, help='Number of experiment')
-	parser.add_argument('--num_device', type=int, default=1, help='Number of GPU, change to 0 if not using CPU')
 	return parser.parse_args()
 
 
 def run(num_exp):
-	def worker():
-		predictor = Predictor(args)
+	subprocess.call('rm ' + args.model_dir + '*', shell=True)
+	predictor = Predictor(args)
+
+	f1_list, jc_list, nmi_list = [], [], []
+	for i in range(num_exp):
 		predictor.train()
 		f1, jc, nmi = predictor.evaluate()
-		queue.put((f1, jc, nmi))
-
-	subprocess.call('rm ' + args.model_dir + '*', shell=True)
-	f1_list, jc_list, nmi_list = [], [], []
-	queue = Queue()
-	processes = []
-	batch_processes = []
-	for i in range(num_exp):
-		print 'process %d' % i
-		device_id = -1 if local_args.num_device == 0 else i % local_args.num_device
-		if local_args.num_device != 0:
-			args.device = device_id
-		process = Process(target=worker)
-		process.start()
-		processes.append(process)
-		if local_args.num_device != 0:
-			batch_processes.append(process)
-		if local_args.num_device != 0 and len(batch_processes) == local_args.num_device:
-			for process in batch_processes:
-				process.join()
-			batch_processes = []
-	for process in processes:
-		process.join()
-
-	for _ in processes:
-		f1, jc, nmi = queue.get()
 		f1_list.append(f1)
 		jc_list.append(jc)
 		nmi_list.append(nmi)
